e4eb321ab67f14b5ad08a28271a0ac53d2849507
unknown <glodybulambo@gmail.com>
2023-06-05 18:55:47 +0200

    adding favicon

--- src/Containers/AboutUs.jsx
+++ src/Containers/AboutUs.jsx
@@ -5,10 +5,10 @@ import AboutCard from '../Components/AboutCard'
 
 const AboutUs = () => {
   return (
-    <div className='relative px-4 lg:px-20 bg-bg py-16 text-white -skew-y-2 rounded-tl-[40px]'>
+    <div className='relative px-4 lg:px-20 bg-bg py-16 text-white -skew-y-3 rounded-tl-[40px]'>
         <div className='w-[80px] h-[80px] bg-blog rounded-full absolute -top-[80px] right-0 z-10'></div>
         <div className='w-[40px] h-[40px] bg-bg absolute -top-[39px] right-0'></div>
-        <div className="skew-y-2">
+        <div className="skew-y-3">
             <h3>About Us</h3>
             <h1>With Successfull Career in the Bealth and Beauty Industry</h1>
             <div className="flex justify-around items-center">
--- src/Containers/AfterCare.jsx
+++ src/Containers/AfterCare.jsx
@@ -9,7 +9,7 @@ const AfterCare = () => {
     <div className="px-4 lg:px-20 bg-blog pt-[220px] -mb-[70px]">
         <h4 className="text-text py-2 font-bold text-xl">AFTERCARE</h4>
         <h2 className='font-Poppins text-2xl md:text-4xl text-bg font-bold md:mb-8'>Permanent Makeup Aftercare.</h2>
-        <div className='flex flex-col-reverse md:flex-row items-center justify-center md:justify-start '>
+        <div className='flex flex-col-reverse md:flex-row items-end justify-center md:justify-start mb-8'>
             <div className="flex-1 ">
                 <img src={aftercareImg} alt="new_girl_pic" />
             </div>
